1a033f76eb84a7714cdadf3da5683c5f2ddbd872
Yin-Hsun Huang <yhh303@nyu.edu>
2018-11-21 01:33:14 -0500

    Add different models

--- leela/main.py
+++ leela/main.py
@@ -11,10 +11,9 @@ import os
 import argparse
 
 import wandb
-wandb.init()
 
 from chunkparser import ChunkParser
-from model import Net, NetNoVal
+from model import Net, NetNoVal, NetNoPol
 
 import torch
 from torch.autograd import Variable
@@ -24,26 +23,26 @@ import torch.nn.functional as F
 # You need to adjust the learning rate if you change this. Should be
 # a multiple of RAM_BATCH_SIZE. NB: It's rare that large batch sizes are
 # actually required.
-# BATCH_SIZE = 512
+BATCH_SIZE = 512
 # Number of examples in a GPU batch. Higher values are more efficient.
 # The maximum depends on the amount of RAM in your GPU and the network size.
 # Must be smaller than BATCH_SIZE.
-RAM_BATCH_SIZE = 128
+RAM_BATCH_SIZE = 256
 
 # Use a random sample input data read. This helps improve the spread of
 # games in the shuffle buffer.
 DOWN_SAMPLE = 1
 
 FILTER_SIZE = 32
-NUM_LAYER = 39
+NUM_LAYER = 19
 
-wandb.config.FILTER_SIZE = FILTER_SIZE
-wandb.config.NUM_BLOCK = NUM_LAYER
-wandb.config.BATCH_SIZE = RAM_BATCH_SIZE
+
 
 device = torch.device("cuda")
 
 parser = argparse.ArgumentParser(description="PyTorch Go NN Trainer")
+parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
+                    help='learning rate (default: 0.01)')
 parser.add_argument('--train-data', type=str, default='modern_games/train_',
                     help="prefix of training data")
 parser.add_argument('--test-data', type=str, default='modern_games/test_',
@@ -52,8 +51,17 @@ parser.add_argument('--checkpoints', type=str, default='checkpoints/',
                     help="prefix checkpoint folder")
 parser.add_argument('--model', type=str, default=None, metavar='MOD',
                 help='if provided, load the model')
+parser.add_argument('--remote-log', action='store_true',
+                help='if provided, do wandb')
 
 args = parser.parse_args()
+
+if args.remote_log:
+    wandb.init()
+
+    wandb.config.FILTER_SIZE = FILTER_SIZE
+    wandb.config.NUM_BLOCK = NUM_LAYER
+    wandb.config.BATCH_SIZE = RAM_BATCH_SIZE
 
 def get_chunks(data_prefix):
     return glob.glob(data_prefix + "*.gz")
@@ -88,19 +96,23 @@ writer = SummaryWriter()
 def compute_loss(prob, winner, target_prob, target_winner):
     if winner is None:
         return (-torch.sum(target_prob * prob)) / RAM_BATCH_SIZE
+    if prob is None:
+        return    F.mse_loss(winner, target_winner.unsqueeze(1))
+    return (-torch.sum(target_prob * prob)/ RAM_BATCH_SIZE +
+            F.mse_loss(winner, target_winner.unsqueeze(1))
+            ) 
 
-    return (-torch.sum(target_prob * prob) +
-            F.mse_loss(target_winner.unsqueeze(1), winner, reduction='sum')
-            ) / RAM_BATCH_SIZE
-
-def train_loop(train_data, test_data, model, info_batch=100, eval_batch=8000):
-    optimizer = torch.optim.SGD(model.parameters() ,lr=1e-2, momentum=0.9, weight_decay=1e-4)
+def train_loop(train_data, test_data, model, macro_batch=1, info_batch=100, eval_batch=8000):
+    optimizer = torch.optim.SGD(model.parameters() ,lr=args.lr, momentum=0.9, weight_decay=1e-4)
+    # optimizer = torch.optim.Adam(model.parameters() ,lr=1e-3, betas=(0.9, 0.999), weight_decay=1e-4)
     scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, 
-                                    milestones=[200000, 400000, 600000, 700000, 800000], gamma=0.1)
+                                    milestones=[50000, 100000, 150000, 200000, 250000], gamma=0.1)
+                                    # milestones=[10000, 20000, 30000, 40000, 50000], gamma=0.1)
 
     step = 1
     
     correct = 0
+    winner_correct = 0
     total_loss = 0
     start = time.time()
     struct_winner = str(RAM_BATCH_SIZE)+'f'
@@ -117,19 +129,15 @@ def train_loop(train_data, test_data, model, info_batch=100, eval_batch=8000):
         probs = np.frombuffer(probs, dtype=np.float32).reshape((RAM_BATCH_SIZE, 362))
         winner = struct.unpack(struct_winner, winner)
         
-        # label = np.argmax(probs, axis=1)
 
         planes = Variable(torch.from_numpy(planes.astype(np.float32))).to(device)
         probs  = Variable(torch.from_numpy(probs)).to(device)
 
         winner = Variable(torch.FloatTensor(winner)).to(device)
         
-        # label = Variable(torch.from_numpy(label.astype(np.int64))).to(device)
-        
         output_prob, output_val = model(planes)
         
-        # loss = F.kl_div(output_prob, probs)
-        # loss = F.nll_loss(output_prob, label)
+        
         loss = compute_loss(output_prob, output_val, probs, winner)
         loss.backward()
         writer.add_scalar('train_loss', loss.item(), step)
@@ -137,73 +145,102 @@ def train_loop(train_data, test_data, model, info_batch=100, eval_batch=8000):
         total_loss += loss.data.cpu().item()
         # accuracy
 
-        pred = output_prob.data.max(1, keepdim=True)[1]
-        label = probs.data.max(1, keepdim=True)[1]
+        if output_prob is not None:
+            pred = output_prob.data.max(1, keepdim=True)[1]
+            label = probs.data.max(1, keepdim=True)[1]
 
-        correct += pred.eq(label.view_as(pred)).cpu().sum().item()
-        #if step % macro_batch_size == 0:
-        optimizer.step()
-        # optimizer.zero_grad()
+            correct += pred.eq(label.view_as(pred)).cpu().sum().item()
+
+        # accuracy of winner
+        if output_val is not None:
+            pred_val = output_val.data.sign()
+            winner_correct += pred_val.eq(winner.view_as(pred_val)).cpu().sum().item()
+        
+        if step % macro_batch == 0:
+            optimizer.step()
+            optimizer.zero_grad()
         
         if step % info_batch == 0:
-            print('Time:{:.3f} Train Step: {} \tLoss: {:.6f} Acc.: {:.6f}%'.format(
+            print('Time:{:.3f} Train Step: {} \tLoss: {:.6f} Acc(next, winner): {:.6f}% {:.6f}%'.format(
                 time.time() - start,
-                step, total_loss/ info_batch, correct / RAM_BATCH_SIZE/info_batch*100.0))
+                step, total_loss/ info_batch, correct / RAM_BATCH_SIZE/info_batch*100.0,
+                winner_correct / RAM_BATCH_SIZE/info_batch*100.0))
             writer.add_scalar('summary/train_loss', total_loss/ info_batch, step)
             writer.add_scalar('summary/train_acc', correct / RAM_BATCH_SIZE/info_batch, step)
-            wandb.log({"loss": total_loss/ info_batch, 'acc': correct / RAM_BATCH_SIZE/info_batch})
+            writer.add_scalar('summary/train_acc_winner', winner_correct / RAM_BATCH_SIZE/info_batch, step)
+            for name, param in model.named_parameters():
+                writer.add_histogram("variable/"+name, param.clone().cpu().data.numpy(), step)
+                writer.add_histogram("gradient/"+name, param.grad.clone().cpu().data.numpy(), step)
             for param_group in optimizer.param_groups:
                 writer.add_scalar('summary/lr', param_group['lr'], step)
+                if args.remote_log:
+                    wandb.log({
+                        "lr": param_group['lr'],
+                        "loss": total_loss/ info_batch, 
+                        'acc': correct / RAM_BATCH_SIZE/info_batch,
+                        'train_acc_winner': winner_correct / RAM_BATCH_SIZE/info_batch})
             total_loss = 0
             correct = 0
-        step+=1
-        if step % eval_batch == (eval_batch-1):
+            winner_correct = 0
+        if step % eval_batch == 0:
             print("Start Evaluating")
             with torch.no_grad():
                 eval_size = 800
                 model.eval()
                 eval_loss = 0
                 eval_correct = 0
-                for i in range(eval_size):
+                eval_correct_winner = 0
+                for _ in range(eval_size):
                     (planes, probs, winner) = next(test_data)
 
                     planes = np.frombuffer(planes, dtype=np.uint8).reshape((RAM_BATCH_SIZE, 18, 19, 19))
                     probs = np.frombuffer(probs, dtype=np.float32).reshape((RAM_BATCH_SIZE, 362))
-                    # label = np.argmax(probs, axis=1)
 
                     planes = Variable(torch.from_numpy(planes.astype(np.float32))).to(device)
                     probs  = Variable(torch.from_numpy(probs)).to(device)
-                    # label = Variable(torch.from_numpy(label.astype(np.int64))).to(device)
+
                     winner = struct.unpack(struct_winner, winner)        
                     winner = Variable(torch.FloatTensor(winner)).to(device)
                     
                     
                     output_prob, output_val = model(planes)
 
-                    #loss = F.kl_div(output, probs)
                     loss = compute_loss(output_prob, output_val, probs, winner)
 
                     eval_loss += loss.data.cpu().item()
+                    
                     # accuracy
+                    if output_prob is not None:
+                        pred = output_prob.data.max(1, keepdim=True)[1]
+                        label = probs.data.max(1, keepdim=True)[1]
 
-                    pred = output_prob.data.max(1, keepdim=True)[1]
-                    label = probs.data.max(1, keepdim=True)[1]
+                        eval_correct += pred.eq(label.view_as(pred)).cpu().sum().item()
+                    
+                    # accuracy of winner
+                    if output_val is not None:
+                        pred_val = output_val.data.sign()
+                        eval_correct_winner += pred_val.eq(winner.view_as(pred_val)).cpu().sum().item()
 
-                    eval_correct += pred.eq(label.view_as(pred)).cpu().sum().item()
-                print('Time:{:.3f} Eval Step: {} \tLoss: {:.6f} Acc.: {:.6f}%'.format(
+                print('Time:{:.3f} Eval Step: {} \tLoss: {:.6f} Acc(next, winner): {:.6f}% {:.6f}%'.format(
                     time.time() - start,
-                    step, eval_loss/eval_size, eval_correct / RAM_BATCH_SIZE/eval_size*100.0))
+                    step, eval_loss/eval_size, eval_correct / RAM_BATCH_SIZE / eval_size*100.0,
+                    eval_correct_winner / RAM_BATCH_SIZE / eval_size*100.0))
                 
                 writer.add_scalar('summary/val_loss', eval_loss/eval_size, step)
                 writer.add_scalar('summary/val_acc', eval_correct / RAM_BATCH_SIZE/eval_size, step)
-                wandb.log({"val_loss": eval_loss/eval_size, 'val_acc': eval_correct / RAM_BATCH_SIZE/eval_size})
+                writer.add_scalar('summary/val_acc_winner', eval_correct_winner / RAM_BATCH_SIZE / eval_size, step)
+                if args.remote_log:
+                
+                    wandb.log({"val_loss": eval_loss/eval_size, 
+                    'val_acc': eval_correct / RAM_BATCH_SIZE/eval_size,
+                    'val_acc_winner': eval_correct_winner / RAM_BATCH_SIZE / eval_size})
 
                 model_file =  'model_' + str(step) + '.pth'
                 torch.save(model.state_dict(), args.checkpoints + model_file)
-                torch.save(model.state_dict(), os.path.join(wandb.run.dir, model_file))
                 print('\nSaved model to ' + model_file + '.')
+        step+=1
 
-model = Net(FILTER_SIZE, NUM_LAYER)
+model = NetNoPol(FILTER_SIZE, NUM_LAYER)
 
 def main(args):
     training = get_chunks(args.train_data)
@@ -223,8 +260,10 @@ def main(args):
         model.load_state_dict(torch.load(args.model))
 
     model.to(device)
-    wandb.hook_torch(model, log='all')
-    train_loop(training_parser, testing_parser, model)
+    if args.remote_log:
+        wandb.hook_torch(model, log='all')
+    
+    train_loop(training_parser, testing_parser, model, macro_batch=BATCH_SIZE/RAM_BATCH_SIZE)
 
 if __name__=='__main__':
     try:
--- leela/model.py
+++ leela/model.py
@@ -11,6 +11,9 @@ class ResConv(nn.Module):
         self.conv1 = nn.Conv2d(FILTER_SIZE, FILTER_SIZE, 3, stride=1, padding=1, bias=False)
         self.conv2 = nn.Conv2d(FILTER_SIZE, FILTER_SIZE, 3, stride=1, padding=1, bias=False)
 
+        nn.init.xavier_uniform_(self.conv1.weight)
+        nn.init.xavier_uniform_(self.conv2.weight)
+
         self.bn1 = nn.BatchNorm2d(FILTER_SIZE)
         self.bn2 = nn.BatchNorm2d(FILTER_SIZE)
 
@@ -26,21 +29,31 @@ class Net(nn.Module):
     def __init__(self, filters=256, layernum=2):
         super(Net, self).__init__()
         FILTER_SIZE = filters
-        self.conv1 = nn.Conv2d(18, FILTER_SIZE, 3, stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(18, FILTER_SIZE, 3, stride=1, padding=0, bias=False)
+        nn.init.xavier_uniform_(self.conv1.weight)
+
         self.bn1 = nn.BatchNorm2d(FILTER_SIZE)
 
         self.res_tower = nn.ModuleList([ResConv(FILTER_SIZE) for i in range(layernum)])
 
         # policy head
         self.conv_pol = nn.Conv2d(FILTER_SIZE, 2, 1, bias=False)
+        nn.init.xavier_uniform_(self.conv_pol.weight)
+
         self.bn_pol = nn.BatchNorm2d(2)
         self.fc_pol = nn.Linear(722, NUM_CLASS)
+        nn.init.xavier_uniform_(self.fc_pol.weight)
 
         # value head
         self.conv_val = nn.Conv2d(FILTER_SIZE, 1, 1, bias=False)
+        nn.init.xavier_uniform_(self.conv_val.weight)
+
         self.bn_val = nn.BatchNorm2d(1)
         self.fc_val_1 = nn.Linear(361, 256)
+        nn.init.xavier_uniform_(self.fc_val_1.weight)
+
         self.fc_val_2 = nn.Linear(256, 1)
+        nn.init.xavier_uniform_(self.fc_val_2.weight)
 
 
     def forward(self, x):
@@ -69,6 +82,61 @@ class Net(nn.Module):
 
 
         return F.log_softmax(pol, dim=1), torch.tanh(val)
+
+    def inference(self, x):
+        """
+        args
+            game is a np matrix of shape (18, 19, 19)
+        return
+            a numpy array probability distribution over 362 moves
+        """
+        with torch.no_grad():
+            output = self.forward(x)
+            return output
+
+class NetNoPol(nn.Module):
+    def __init__(self, filters=256, layernum=2):
+        super(NetNoPol, self).__init__()
+        FILTER_SIZE = filters
+        self.conv1 = nn.Conv2d(18, FILTER_SIZE, 3, stride=1, padding=1, bias=False)
+        nn.init.xavier_uniform_(self.conv1.weight)
+
+        self.bn1 = nn.BatchNorm2d(FILTER_SIZE)
+
+        self.res_tower = nn.ModuleList([ResConv(FILTER_SIZE) for i in range(layernum)])
+
+
+        # value head
+        self.conv_val = nn.Conv2d(FILTER_SIZE, 1, 1, bias=False)
+        nn.init.xavier_uniform_(self.conv_val.weight)
+
+        self.bn_val = nn.BatchNorm2d(1)
+        self.fc_val_1 = nn.Linear(361, 256)
+        nn.init.xavier_uniform_(self.fc_val_1.weight)
+
+        self.fc_val_2 = nn.Linear(256, 1)
+        nn.init.xavier_uniform_(self.fc_val_2.weight)
+
+
+    def forward(self, x):
+        """
+        x is (batch, 18, 19, 19)
+        """
+        net = self.conv1(x)
+        net = F.relu(self.bn1(net))
+
+        for layer in self.res_tower:
+            net = layer(net)
+
+        # value head
+        val = self.conv_val(net)
+        val = F.relu(self.bn_val(val))
+        val = val.view(-1, 361)
+        val = F.relu(self.fc_val_1(val))
+        val = self.fc_val_2(val)
+
+
+        return None , torch.tanh(val)
 
     def inference(self, x):
         """
