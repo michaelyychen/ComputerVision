cd477bc053af53ade028741fc988f5bf9f6752d8
Michael <michaelyychen@nyu.edu>
2018-12-21 22:34:30 -0500

    update

--- model.py
+++ model.py
@@ -2,79 +2,494 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-nclasses = 361 # 19*19 
-channel_size = 64
-class BasicResBlock(nn.Module):
-    def __init__(self, in_dim,out_dim, dropout = None, kernel_size=3, padding=1):
-        super(BasicResBlock, self).__init__()
-        self.dropout_rate = dropout
-        self.is_dim_equal = (in_dim == out_dim)
-        self.bn1 = nn.BatchNorm2d(in_dim)
-        self.conv1 = nn.Conv2d(in_dim, out_dim, kernel_size=kernel_size,padding=padding)
+from layer import *
 
-        self.bn2 = nn.BatchNorm2d(out_dim)
-        self.conv2 = nn.Conv2d(out_dim, out_dim, kernel_size=kernel_size,padding=padding)
+NUM_CLASS = 19*19+1
+
+###
+###   This is the Residual Tower Model, including Baseline, Res 19, Res39
+###   no winner prediction
+###   Michael USE THIS
+class Net_Baseline(nn.Module):
+    def __init__(self, filters=256, layernum=2):
+        super(Net_Baseline, self).__init__()
+        FILTER_SIZE = filters
+        self.conv1 = nn.Conv2d(18, FILTER_SIZE, 3, stride=1, padding=1, bias=True)
+        nn.init.xavier_uniform_(self.conv1.weight)
+
+        self.bn1 = nn.BatchNorm2d(FILTER_SIZE)
+
+        self.res_tower = nn.ModuleList([ResConv(FILTER_SIZE) for i in range(layernum)])
+
+        # policy head
+        self.conv_pol = nn.Conv2d(FILTER_SIZE, 2, 1, bias=True)
+        nn.init.xavier_uniform_(self.conv_pol.weight)
+
+        self.bn_pol = nn.BatchNorm2d(2)
+        self.fc_pol = nn.Linear(722, NUM_CLASS)
+        nn.init.xavier_uniform_(self.fc_pol.weight)
+
+        # value head
+        # self.conv_val = nn.Conv2d(FILTER_SIZE, 1, 1, bias=True)
+        # nn.init.xavier_uniform_(self.conv_val.weight)
+
+        # self.bn_val = nn.BatchNorm2d(1)
+        # self.fc_val_1 = nn.Linear(361, 256)
+        # nn.init.xavier_uniform_(self.fc_val_1.weight)
+
+        # self.fc_val_2 = nn.Linear(256, 1)
+        # nn.init.xavier_uniform_(self.fc_val_2.weight)
+
+
+    def forward(self, x):
+        """
+        x is (batch, 18, 19, 19)
+        """
+        net = self.conv1(x)
+        net = F.relu(self.bn1(net))
+
+        for layer in self.res_tower:
+            net = layer(net)
+        
+
+
+        pol = self.conv_pol(net)
+        pol = F.relu(self.bn_pol(pol))
+        pol = pol.view(-1, 722)
+        pol = self.fc_pol(pol)
+
+        # value head
+        # val = self.conv_val(net)
+        # val = F.relu(self.bn_val(val))
+        # val = val.view(-1, 361)
+        # val = F.relu(self.fc_val_1(val))
+        # val = self.fc_val_2(val)
+
+
+        return F.log_softmax(pol, dim=1), None
+    def inference(self, x):
+        """
+        args
+            game is a np matrix of shape (18, 19, 19)
+        return
+            a numpy array probability distribution over 362 moves
+        """
+        with torch.no_grad():
+            output = self.forward(x)
+            return output
+
+class NetV2(nn.Module):
+    def __init__(self, filters=256, layernum=2):
+        super(NetV2, self).__init__()
+        FILTER_SIZE = filters
+        self.conv1 = nn.Conv2d(18, FILTER_SIZE, 3, stride=1, padding=1, bias=False)
+        nn.init.xavier_uniform_(self.conv1.weight)
+
+        self.bn1 = nn.BatchNorm2d(FILTER_SIZE)
+
+        self.res_tower = nn.ModuleList([ResConvAttn(FILTER_SIZE, fold=4) for i in range(layernum)])
+
+        # policy head
+        self.conv_pol = nn.Conv2d(FILTER_SIZE, 2, 1, bias=False)
+        nn.init.xavier_uniform_(self.conv_pol.weight)
+
+        self.bn_pol = nn.BatchNorm2d(2)
+        self.fc_pol = nn.Linear(722, NUM_CLASS)
+        nn.init.xavier_uniform_(self.fc_pol.weight)
+
+        # value head
+        self.conv_val = nn.Conv2d(FILTER_SIZE, 1, 1, bias=False)
+        nn.init.xavier_uniform_(self.conv_val.weight)
+
+        self.bn_val = nn.BatchNorm2d(1)
+        self.fc_val_1 = nn.Linear(361, 256)
+        nn.init.xavier_uniform_(self.fc_val_1.weight)
+
+        self.fc_val_2 = nn.Linear(256, 1)
+        nn.init.xavier_uniform_(self.fc_val_2.weight)
+
+
+    def forward(self, x):
+        """
+        x is (batch, 18, 19, 19)
+        """
+        net = self.conv1(x)
+        net = self.bn1(net)
+        net = F.relu(net)
+
+        for layer in self.res_tower:
+            net = layer(net)
+
+        # net = F.relu(net)
+
+        pol = self.conv_pol(net)
+        pol = F.relu(self.bn_pol(pol))
+        pol = pol.view(-1, 722)
+        pol = self.fc_pol(pol)
+
+        # value head
+        val = self.conv_val(net)
+        val = F.relu(self.bn_val(val))
+        val = val.view(-1, 361)
+        val = F.relu(self.fc_val_1(val))
+        val = self.fc_val_2(val)
+
+
+        return F.log_softmax(pol, dim=1), torch.tanh(val)
+
+    def inference(self, x):
+        """
+        args
+            game is a np matrix of shape (18, 19, 19)
+        return
+            a numpy array probability distribution over 362 moves
+        """
+        with torch.no_grad():
+            output = self.forward(x)
+            return output
+
+class NetV3(nn.Module):
+    def __init__(self, filters=256):
+        super(NetV3, self).__init__()
+        FILTER_SIZE = filters
+        self.filter_size = filters
+        self.conv1 = nn.Conv2d(18, FILTER_SIZE, 3, stride=1, padding=1, bias=False)
+        nn.init.xavier_uniform_(self.conv1.weight)
+
+        self.bn1 = nn.BatchNorm2d(FILTER_SIZE)
+
+        self.res_tower = nn.ModuleList([ResConv(FILTER_SIZE) for i in range(9)])
+        self.attn_tower = nn.ModuleList([TransformerBlock(FILTER_SIZE) for _ in range(3)])
+        # policy head
+        self.conv_pol = nn.Conv2d(FILTER_SIZE, 2, 1, bias=False)
+        nn.init.xavier_uniform_(self.conv_pol.weight)
+
+        self.bn_pol = nn.BatchNorm2d(2)
+        self.fc_pol = nn.Linear(722, NUM_CLASS)
+        nn.init.xavier_uniform_(self.fc_pol.weight)
+
+        # value head
+        self.conv_val = nn.Conv2d(FILTER_SIZE, 1, 1, bias=False)
+        nn.init.xavier_uniform_(self.conv_val.weight)
+
+        self.bn_val = nn.BatchNorm2d(1)
+        self.fc_val_1 = nn.Linear(361, 256)
+        nn.init.xavier_uniform_(self.fc_val_1.weight)
+
+        self.fc_val_2 = nn.Linear(256, 1)
+        nn.init.xavier_uniform_(self.fc_val_2.weight)
+
+
+    def forward(self, x):
+        """
+        x is (batch, 18, 19, 19)
+        """
+        net = self.conv1(x)
+        net = self.bn1(net)
+        net = F.relu(net)
+
+        for layer in self.res_tower:
+            net = layer(net)
+
+        net = net.reshape((-1, self.filter_size, 361))
+        net = net.permute(0, 2, 1)
+
+        for layer in self.attn_tower:
+            net = layer(net)
+
+        net = net.permute(0, 2, 1)
+        net = net.reshape((-1, self.filter_size, 19, 19))
+
+        # net = F.relu(net)
+
+        pol = self.conv_pol(net)
+        pol = F.relu(self.bn_pol(pol))
+        pol = pol.view(-1, 722)
+        pol = self.fc_pol(pol)
+
+        # value head
+        val = self.conv_val(net)
+        val = F.relu(self.bn_val(val))
+        val = val.view(-1, 361)
+        val = F.relu(self.fc_val_1(val))
+        val = self.fc_val_2(val)
+
+
+        return F.log_softmax(pol, dim=1), torch.tanh(val)
+
+    def inference(self, x):
+        """
+        args
+            game is a np matrix of shape (18, 19, 19)
+        return
+            a numpy array probability distribution over 362 moves
+        """
+        with torch.no_grad():
+            output = self.forward(x)
+            return output
+
+class NetV4(nn.Module):
+    def __init__(self, filters=256):
+        super(NetV4, self).__init__()
+        FILTER_SIZE = filters
+        self.filter_size = filters
+        self.conv1 = nn.Conv2d(18, FILTER_SIZE, 3, stride=1, padding=1, bias=False)
+        nn.init.xavier_uniform_(self.conv1.weight)
+
+        self.bn1 = nn.BatchNorm2d(FILTER_SIZE)
+
+        self.res_tower = nn.ModuleList([ResConv(FILTER_SIZE) for i in range(9)])
+        self.attn_tower = nn.ModuleList([TransformerBlock(FILTER_SIZE) for _ in range(3)])
+        
+        # policy head
+        self.conv_pol = nn.Conv2d(FILTER_SIZE, 2, 1, bias=False)
+        nn.init.xavier_uniform_(self.conv_pol.weight)
+
+        self.bn_pol = nn.BatchNorm2d(2)
+        self.fc_pol = nn.Linear(722, NUM_CLASS)
+        nn.init.xavier_uniform_(self.fc_pol.weight)
+
+        # value head
+        self.conv_val = nn.Conv2d(FILTER_SIZE, 1, 1, bias=False)
+        nn.init.xavier_uniform_(self.conv_val.weight)
+
+        self.bn_val = nn.BatchNorm2d(1)
+        self.fc_val_1 = nn.Linear(361, 256)
+        nn.init.xavier_uniform_(self.fc_val_1.weight)
+
+        self.fc_val_2 = nn.Linear(256, 1)
+        nn.init.xavier_uniform_(self.fc_val_2.weight)
+
+
+    def forward(self, x):
+        """
+        x is (batch, 18, 19, 19)
+        """
+        net = self.conv1(x)
+        net = self.bn1(net)
+        net = F.relu(net)
+
+        for layer in self.res_tower:
+            net = layer(net)
+
+        pol = self.conv_pol(net)
+        pol = F.relu(self.bn_pol(pol))
+        pol = pol.view(-1, 722)
+        pol = self.fc_pol(pol)
+
+        net = net.reshape((-1, self.filter_size, 361))
+        net = net.permute(0, 2, 1)
+
+        for layer in self.attn_tower:
+            net = layer(net)
+
+        net = net.permute(0, 2, 1)
+        net = net.reshape((-1, self.filter_size, 19, 19))
+
+        # value head
+        val = self.conv_val(net)
+        val = F.relu(self.bn_val(val))
+        val = val.view(-1, 361)
+        val = F.relu(self.fc_val_1(val))
+        val = self.fc_val_2(val)
+
+
+        return F.log_softmax(pol, dim=1), torch.tanh(val)
+
+    def inference(self, x):
+        """
+        args
+            game is a np matrix of shape (18, 19, 19)
+        return
+            a numpy array probability distribution over 362 moves
+        """
+        with torch.no_grad():
+            output = self.forward(x)
+            return output
+
+class NetV5(nn.Module):
+    def __init__(self, filters=256):
+        super(NetV5, self).__init__()
+        FILTER_SIZE = filters
+        self.filter_size = filters
+        self.conv1 = nn.Conv2d(18, FILTER_SIZE, 3, stride=1, padding=1, bias=False)
+        nn.init.xavier_uniform_(self.conv1.weight)
+
+        self.bn1 = nn.BatchNorm2d(FILTER_SIZE)
+
+        self.res_tower = nn.ModuleList([ResConv(FILTER_SIZE) for _ in range(3)])
+        self.attn_tower = nn.ModuleList([TransformerBlock(FILTER_SIZE) for _ in range(3)])
+        self.attn_2 = TransformerBlock(FILTER_SIZE)
+        
+        # policy head
+        self.conv_pol = nn.Conv2d(FILTER_SIZE, 2, 1, bias=False)
+        nn.init.xavier_uniform_(self.conv_pol.weight)
+
+        self.bn_pol = nn.BatchNorm2d(2)
+        self.fc_pol = nn.Linear(722, NUM_CLASS)
+        nn.init.xavier_uniform_(self.fc_pol.weight)
+
+        # value head
+        self.conv_val = nn.Conv2d(FILTER_SIZE, 1, 1, bias=False)
+        nn.init.xavier_uniform_(self.conv_val.weight)
+
+        self.bn_val = nn.BatchNorm2d(1)
+        self.fc_val_1 = nn.Linear(361, 256)
+        nn.init.xavier_uniform_(self.fc_val_1.weight)
+
+        self.fc_val_2 = nn.Linear(256, 1)
+        nn.init.xavier_uniform_(self.fc_val_2.weight)
+
+
+    def forward(self, x):
+        """
+        x is (batch, 18, 19, 19)
+        """
+        net = self.conv1(x)
+        net = self.bn1(net)
+        net = F.relu(net)
+
+        for layer in self.res_tower:
+            net = layer(net)
 
         
-        if(self.is_dim_equal):
-            self.convShort = None
-        else:
-            self.convShort = nn.Conv2d(in_dim, out_dim,padding=0, kernel_size=1,bias = False)
 
-    def forward(self, x):
-        if(self.is_dim_equal):
-            x_w = self.conv1(F.relu(self.bn1(x)))
-        else:
-            x = F.relu(self.bn1(x))
-            x_w = self.conv1(x)
+        net = net.reshape((-1, self.filter_size, 361))
+        net = net.permute(0, 2, 1)
 
-        if self.dropout_rate is not None:
-            x_w = F.dropout2d(x_w,p = self.dropout_rate, training=self.training )
-        x_w = self.conv2(F.relu(self.bn2(x_w)))
-        if self.is_dim_equal:
-            return x_w+x
-        else:
-            return x_w+self.convShort(x)
-
-class ResBlock(nn.Module):
-    def __init__(self, num_of_layers, in_dim, dropout = None, kernel_size=3, padding=1 ):
-        super(ResBlock, self).__init__()
-        self.layer = self.create_layer(num_of_layers, in_dim,dropout = dropout,kernel_size=kernel_size, padding=padding )
-    def create_layer(self, num_of_layers, in_dim, dropout = None,kernel_size=3, padding=1):
-        layers = []
-        for i in range(int(num_of_layers)):
-            layers.append(BasicResBlock(in_dim,in_dim,dropout = dropout,kernel_size=kernel_size, padding=padding))
-        return nn.Sequential(*layers)
-    def forward(self, x):
-        return self.layer(x)
-
-class GoNet(nn.Module):
-    def __init__(self, inChannel):
-        super(GoNet, self).__init__()
-        self.res1 = BasicResBlock(inChannel,channel_size,kernel_size=5, padding=2)
-
-        self.block1 = ResBlock(10,channel_size,kernel_size=5, padding=2)
-        self.block2 = ResBlock(6,channel_size,kernel_size=3, padding=1)
-
-        self.res2 =  BasicResBlock(channel_size,1,kernel_size=3, padding=1)
- 
-
-    def num_flat_features(self, x):
-        size = x.size()[1:]  # all dimensions except the batch dimension\n",
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
-
-    def forward(self, x):
+        net_attn_1 = net
+        for layer in self.attn_tower:
+            net_attn_1 = layer(net_attn_1)
         
-        x = self.res1(x)
+        net = self.attn_2(net_attn_1)
 
-        x = self.block1(x)
-        x = self.block2(x)
+        net_attn_1 = net_attn_1.permute(0, 2, 1)
+        net_attn_1 = net_attn_1.reshape((-1, self.filter_size, 19, 19))
 
-        x = self.res2(x)
+        # value head
+        val = self.conv_val(net_attn_1)
+        val = F.relu(self.bn_val(val))
+        val = val.view(-1, 361)
+        val = F.relu(self.fc_val_1(val))
+        val = self.fc_val_2(val)
 
-        x = x.view(-1, self.num_flat_features(x))
+        
+        net = net.permute(0, 2, 1)
+        net = net.reshape((-1, self.filter_size, 19, 19))
 
-        return F.log_softmax(x,dim=1)
+        pol = self.conv_pol(net)
+        pol = F.relu(self.bn_pol(pol))
+        pol = pol.view(-1, 722)
+        pol = self.fc_pol(pol)
+
+
+        return F.log_softmax(pol, dim=1), torch.tanh(val)
+
+    def inference(self, x):
+        """
+        args
+            game is a np matrix of shape (18, 19, 19)
+        return
+            a numpy array probability distribution over 362 moves
+        """
+        with torch.no_grad():
+            output = self.forward(x)
+            return output
+
+class NetNoPol(nn.Module):
+    def __init__(self, filters=256, layernum=2):
+        super(NetNoPol, self).__init__()
+        FILTER_SIZE = filters
+        self.conv1 = nn.Conv2d(18, FILTER_SIZE, 3, stride=1, padding=1, bias=True)
+        nn.init.xavier_uniform_(self.conv1.weight)
+
+        self.bn1 = nn.BatchNorm2d(FILTER_SIZE)
+
+        self.res_tower = nn.ModuleList([ResConv(FILTER_SIZE) for i in range(layernum)])
+
+
+        # value head
+        self.conv_val = nn.Conv2d(FILTER_SIZE, 1, 1, bias=True)
+        nn.init.xavier_uniform_(self.conv_val.weight)
+
+        self.bn_val = nn.BatchNorm2d(1)
+        self.fc_val_1 = nn.Linear(361, 256)
+        nn.init.xavier_uniform_(self.fc_val_1.weight)
+
+        self.fc_val_2 = nn.Linear(256, 1)
+        nn.init.xavier_uniform_(self.fc_val_2.weight)
+
+
+    def forward(self, x):
+        """
+        x is (batch, 18, 19, 19)
+        """
+        net = self.conv1(x)
+        net = F.relu(self.bn1(net))
+
+        for layer in self.res_tower:
+            net = layer(net)
+
+        # value head
+        val = self.conv_val(net)
+        val = F.relu(self.bn_val(val))
+        val = val.view(-1, 361)
+        val = F.relu(self.fc_val_1(val))
+        val = self.fc_val_2(val)
+
+
+        return None , torch.tanh(val)
+
+    def inference(self, x):
+        """
+        args
+            game is a np matrix of shape (18, 19, 19)
+        return
+            a numpy array probability distribution over 362 moves
+        """
+        with torch.no_grad():
+            output = self.forward(x)
+            return output
+
+class Net_SimpleConv(nn.Module):
+    def __init__(self, filters=256, layernum=2):
+        super(Net_SimpleConv, self).__init__()
+        FILTER_SIZE = filters
+        self.conv1 = nn.Conv2d(18, FILTER_SIZE, 3, stride=1, padding=1, bias=True)
+        self.bn1 = nn.BatchNorm2d(FILTER_SIZE)
+
+        self.res_tower = nn.ModuleList([ResConv(FILTER_SIZE) for i in range(layernum)])
+
+        # policy head
+        self.conv_pol = nn.Conv2d(FILTER_SIZE, 2, 1, bias=True)
+        self.bn_pol = nn.BatchNorm2d(2)
+        self.fc_pol = nn.Linear(722, NUM_CLASS)
+
+
+    def forward(self, x):
+        """
+        x is (batch, 18, 19, 19)
+        """
+        net = self.conv1(x)
+        net = F.relu(self.bn1(net))
+
+        for layer in self.res_tower:
+            net = layer(net)
+
+        pol = self.conv_pol(net)
+        pol = F.relu(self.bn_pol(pol))
+        pol = pol.view(-1, 722)
+        pol = self.fc_pol(pol)
+
+        return F.log_softmax(pol, dim=1), None
+
+    def inference(self, x):
+        """
+        args
+            game is a np matrix of shape (18, 19, 19)
+        return
+            a numpy array probability distribution over 362 moves
+        """
+        with torch.no_grad():
+            output = self.forward(x)
+            return output
